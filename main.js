ce22699b3eff1e3342f300c5240bbb24221348c6
Dabsia <dabojohnson98@gmail.com>
2022-02-01 15:38:02 +0000

    A form field with vanilla html and JS

---
+++ main.js
@@ -0,0 +1,37 @@
+let Form = document.getElementById('myForm');
+let inputs = document.querySelectorAll('.input__field');
+let pword1 = document.querySelector('.pword1');
+let pword2 = document.querySelector('.pword2');
+let holdingSuccessMessage = document.querySelector('.holdingSuccessMessage');
+let main = document.querySelector('main');
+let ContinueBTN = document.querySelector('.continue')
+
+
+const submitForm = (e) => {
+    e.preventDefault()
+    inputs.forEach((input) => {
+        if (input.value == ''){
+            input.style.border = '1px solid red'
+        }
+        else{
+            input.style.border = '1px solid green'
+            holdingSuccessMessage.style.display = 'flex';
+            holdingSuccessMessage.style.zIndex = '1';
+            main.style.zIndex = '-1';
+
+        }
+    })
+   
+}
+
+ContinueBTN.addEventListener('click', ()=>{
+    holdingSuccessMessage.style.display = 'none'
+    inputs.forEach(input => {
+        input.value = '';
+        input.style.border = '1px solid #121212'
+
+    })
+})
+
+
+Form.addEventListener('submit', submitForm)
